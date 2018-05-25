a9d5f26b900bfcdc3a807c21569c3161ca9c5c9d
marcofolio <marcoroelkuiper@gmail.com>
2018-05-25 13:39:01 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # XamSMS
+
+Source files for the article on [Marcofolio.net](http://www.marcofolio.net/) called "[*First look at Xamarin.Essentials*](https://marcofolio.net/xamarin-essentials-first-look/)" .
+
+Browse the code or read the article to get more information. This sample uses [Xamarin.Essentials](https://github.com/xamarin/Essentials) to send an SMS in a cross-platform manner with [Xamarin.Forms](https://docs.microsoft.com/en-us/xamarin/xamarin-forms/). Demo works on a Android Simulator and physical iOS or Android device.
---
+++ XamSMS/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace XamSMS
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+		private async void submitBtn_Clicked(object sender, EventArgs e)
+        {
+			var message = messageEntry.Text;
+			var recipient = recipientEntry.Text;
+
+			try
+            {
+				var smsMessage = new SmsMessage(message, recipient);
+				await Sms.ComposeAsync(smsMessage);
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                // Sms is not supported on this device.
+            }
+            catch (Exception ex)
+            {
+                // Other error has occurred.
+            }
+        }
+    }
+}
